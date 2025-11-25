ff1cbef8669e444c97f489d9f754f472844cb0d8
ProgrammerVijender <Vijenderkumar4pc@gmail.com>
2025-11-25 16:15:51 +0530

    stack assignment 8 done

---
+++ Stack/Assignments/08_Check_If_Word_Is_Valid_After_Substitutions.cpp
@@ -0,0 +1,50 @@
+// 1003. Check If Word Is Valid After Substitutions
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        for(char i:s)
+        {
+            if( i == 'a')
+            {
+                st.push(i);
+            }
+            else if(i == 'b')
+            {
+                if(!st.empty()  &&  st.top() == 'a' )
+                {
+                    st.push(i);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                if(!st.empty()  &&   st.top() == 'b')
+                {
+                    st.pop();
+                    if(!st.empty()  &&  st.top() == 'a')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+           
+        }
+
+        return (st.empty() ) ? 1:0;
+
+    }
+};
